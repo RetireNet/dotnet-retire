9d57d22c577d4fb16dc8ca07f56c2fdbd91c29fa
John10 Korsnes <johnkors@gmail.com>
2017-05-15 19:52:50 +0200

    Usage finder tests

---
+++ Tests/UsageFinderTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using dotnet_retire;
+using Xunit;
+
+namespace Tests
+{
+    public class UsageFinderTests
+    {
+        [Fact]
+        public void DirectDependencyIsUsage()
+        {
+            var assets = new List<NugetReference>
+            {
+                new NugetReference
+                {
+                    Id = "SomePackageId",
+                    Version = "1.0.0"
+                }
+            };
+
+            var packages = new List<Package>{ new Package
+            {
+                Id = "SomePackageId",
+                Affected = "1.0.0"
+            }};
+            var usages = UsagesFinder.FindUsagesOf(assets, packages);
+            Assert.Equal(1, usages.Count());
+        }
+
+        [Fact]
+        public void TransientDependencyIsUsage()
+        {
+            var assets = new List<NugetReference>
+            {
+                new NugetReference
+                {
+                    Id = "SomeOtherId",
+                    Version = "2.0.0",
+                    Dependencies = new List<NugetReference>
+                    {
+                        new NugetReference
+                        {
+                            Id = "SomePackageId",
+                            Version = "1.0.0"
+                        }
+                    }
+                }
+            };
+
+            var packages = new List<Package>{ new Package
+            {
+                Id = "SomePackageId",
+                Affected = "1.0.0"
+            }};
+
+            var usages = UsagesFinder.FindUsagesOf(assets, packages);
+            Assert.Equal(1, usages.Count());
+            Assert.True(usages.First() is TransientUsage);
+        }
+    }
+}
--- dotnet-retire/Models/Usage.cs
+++ dotnet-retire/Models/Usage.cs
@@ -4,6 +4,5 @@ namespace dotnet_retire
     {
         public NugetReference NugetReference { get; set; }
         public Package Package { get; set; }
-        public string Type { get; set; }
     }
 }
