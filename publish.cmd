dd1ba54aeddb6bd481097aef7d6c40dfc457d815
John10 Korsnes <johnkors@gmail.com>
2017-05-15 23:02:54 +0200

    Set 1.0.0

--- publish.cmd
+++ publish.cmd
@@ -1,1 +1,1 @@
-nuget push dotnet-retire/deploy/dotnet-retire.1.0.0-beta002.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey %nugetorgapikey%
+nuget push dotnet-retire/deploy/dotnet-retire.1.0.0.nupkg -Source https://www.nuget.org/api/v2/package -ApiKey %nugetorgapikey%
