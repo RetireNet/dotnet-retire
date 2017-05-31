ed9982418f1f89c58dd57ee8d393b6e218d8ab4e
john4ice <john.korsnes@ice.net>
2017-05-31 13:57:14 +0200

    Fix scripts so they work from a clean repo

--- pack.cmd
+++ pack.cmd
@@ -1,1 +1,5 @@
+IF EXIST ./deploy RMDIR ./deploy /s /q
+dotnet clean dotnet-retire
+dotnet restore dotnet-retire
+dotnet build dotnet-retire -c Release
 dotnet pack dotnet-retire -o .\deploy -c Release
