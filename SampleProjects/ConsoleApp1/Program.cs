4a7e9633d73553ec1e1b4078dc6ec29cfa51321a
John Korsnes <johnkors@gmail.com>
2017-06-08 22:01:00 +0200

    Create Program.cs

--- SampleProjects/ConsoleApp1/Program.cs
+++ SampleProjects/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Consul.WriteLine("Hello World!");
         }
     }
 }
