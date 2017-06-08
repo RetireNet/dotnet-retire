7bb7e83f6b1aeec68b7b3455dd4370059d18513b
John10 Korsnes <johnkors@gmail.com>
2017-06-08 20:57:49 +0200

    Cleanup some folder structure for pack/deploy

--- pack.cmd
+++ pack.cmd
@@ -1,5 +1,5 @@
-IF EXIST "./dotnet-retire/deploy" RMDIR "./dotnet-retire/deploy" /s /q
+IF EXIST "./deploy" RMDIR "./deploy" /s /q
 dotnet clean dotnet-retire
 dotnet restore dotnet-retire
 dotnet build dotnet-retire -c Release
-dotnet pack dotnet-retire -o .\deploy -c Release
+dotnet pack dotnet-retire -o ..\deploy -c Release
