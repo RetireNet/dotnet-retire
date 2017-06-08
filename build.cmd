b9f4f80fde6d2f2c4df5c82594dafd1cb86b5620
John10 Korsnes <johnkors@gmail.com>
2017-06-08 21:56:32 +0200

    Make build script output errorcodes to AppVeyor

--- build.cmd
+++ build.cmd
@@ -1,5 +1,11 @@
+@echo off
 dotnet restore dotnet-retire.sln
+IF %errorlevel% neq 0 exit /b %errorlevel%
+
 dotnet build dotnet-retire.sln -c Release
+IF %errorlevel% neq 0 exit /b %errorlevel%
+
 cd Tests
 dotnet xunit -c Release
+IF %errorlevel% neq 0 exit /b %errorlevel%
 cd ..
