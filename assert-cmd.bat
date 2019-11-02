9194e5ac7058299807a84987122137c12f3cc2e9
Tobias Sibera <discjoggy@gmx.de>
2019-11-02 16:09:27 +0100

    Fix evaluating wrong exit codes in assert-cmd scripts

--- assert-cmd.bat
+++ assert-cmd.bat
@@ -3,10 +3,11 @@ CD /D %~dp0
 
 echo "### AssertCMD - Executing command '%2 %3'"
 %2 %3
+SET code=%ERRORLEVEL%
 
-if %ERRORLEVEL% neq %1 (
-  echo "### AssertCMD - FAIL: Expected exit code %1 is not equal to %ERRORLEVEL%" 1>&2
-  exit 999
+if %code% neq %1 (
+  echo "### AssertCMD - FAIL: Expected exit code %1 is not equal to %code%" 1>&2
+  exit 1
 )
 
 echo "### AssertCMD - PASSED"
