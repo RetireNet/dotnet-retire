319b0a938336bb2fe507f927335e07f1c4a57b0d
John Korsnes <johnkors@gmail.com>
2021-11-02 14:19:17 +0100

    minimal api for sample

--- SampleProjects/VulnerableRunTimeWebApp/Program.cs
+++ SampleProjects/VulnerableRunTimeWebApp/Program.cs
@@ -1,30 +1,14 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Serilog;
+﻿using Serilog;
 using Serilog.Formatting.Compact;
 
-namespace VulnerableRunTimeWebApp
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRetireRuntimeHostedService(o => o.CheckInterval = 10);
+builder.WebHost.UseSerilog((hostingContext, loggerConfiguration) =>
+    loggerConfiguration
+        .MinimumLevel.Debug()
+        .WriteTo.Console(new CompactJsonFormatter()));
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .ConfigureServices((app, services) =>
-                {
-                    services.AddRetireRuntimeHostedService();
-                })
-                .Configure(app => app.UseRuntimeVulnerabilityReport())
-                .UseSerilog((hostingContext, loggerConfiguration) =>
-                    loggerConfiguration
-                        .ReadFrom.Configuration(hostingContext.Configuration)
-                        .WriteTo.Console(new CompactJsonFormatter()));
+var app = builder.Build();
+app.UseRuntimeVulnerabilityReport();
+await app.RunAsync();
 
-    }
-}
